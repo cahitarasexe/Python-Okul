8afea748822a920f7a3c4cefb2178ee1ff1abfe2
Aras <104190764+cahitarasexe@users.noreply.github.com>
2023-03-28 10:05:09 +0300

    İf-Else-Elif

---
+++ İf-Else-Elif/sınav puanı.py
@@ -0,0 +1,11 @@
+sinavpuani=int(input("Puanınızı Giriniz (0-100):"))
+if sinavpuani>=85:
+    print("Pek İyi")
+elif sinavpuani>=70:
+    print("İyi")
+elif sinavpuani>=55:
+    print("Orta")
+elif sinavpuani>=45:
+    print("Geçer")
+else:
+    print("Kaldı")
---
+++ İf-Else-Elif/tek-çift.py
@@ -0,0 +1,5 @@
+sayi=int(input("Bir Sayı Giriniz:"))
+if sayi%2==0:
+    print("Sayı Çifttir")
+else:
+    print("Sayı Tektir")
